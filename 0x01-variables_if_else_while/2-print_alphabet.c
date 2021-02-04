9e229b0d42f8e1d37891e6b60e2a87b78f934d09
root <root@DESKTOP-SV3BINQ.localdomain>
2021-02-04 16:35:02 -0500

    2 with ln

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	while (c <= 'Z')
+	{
+	putchar(tolower(c));
+	c++;
+	putchar ('\n');
+	}
+	return (0);
+}
