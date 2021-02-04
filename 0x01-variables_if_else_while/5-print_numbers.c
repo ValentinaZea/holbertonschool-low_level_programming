bd379711cd7bae8f96faaa46d911ac28bab2d74b
root <root@DESKTOP-SV3BINQ.localdomain>
2021-02-04 15:48:17 -0500

    Exercise 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("%d\n", a);
+	a++;
+	}
+	return (0);
+}
