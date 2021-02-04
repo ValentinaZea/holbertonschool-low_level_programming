35e590fbbefb503a8d2a27abb59eb6e443abad21
root <root@DESKTOP-SV3BINQ.localdomain>
2021-02-04 00:43:28 -0500

    gdgdgd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 /*
  * main - Size of various types
- * @c: Character type
- * @i: Int type
- * @li: Longint type
- * @lli: Longlong int type
- * @f: Float type
  * Description: Prints the size of various types depending on
  * the computer where is run on
  *
 */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longType;
-	long long int longLongType;
-	float floatType;
+	printf("Size of a char : %lu byte(s)\n", sizeof(char));
+	printf("Size of an int : %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int : %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int : %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float : %lu byte(s)\n", sizeof(float));
 
-	printf("Size of a char : %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int : %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int : %zu byte(s)\n", sizeof(longType));
-	printf("Size of a long long int : %zu byte(s)\n", sizeof(longLongType));
-	printf("Size of a float : %zu byte(s)\n", sizeof(floatType))
 	return (0);
 }
 
